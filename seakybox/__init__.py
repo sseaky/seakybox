2e9d283615c4ffe8762096cc79f401c284376a67
PFJ-I7\seaky <seaky.cn@gmail.com>
2023-05-23 23:49:09 +0800

    0.2

--- seakybox/__init__.py
+++ seakybox/__init__.py
@@ -5,6 +5,6 @@
 
 __title__ = 'seakybox'
 __author__ = 'Seaky'
-__version__ = '0.1'
-__copyright__ = 'Copyright (c) 2022 Seaky'
+__version__ = '0.2'
+__copyright__ = 'Copyright (c) 2023 Seaky'
 __description__ = 'This is Seaky\'s box of basic tools'
